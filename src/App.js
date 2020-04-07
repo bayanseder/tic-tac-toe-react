d33fb66019b387b7a4c0f8df75f1b7bbe05b135d
bayanseder <bayansider88@gmail.com>
2020-04-07 13:55:41 +0300

    2 players

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Boxs from './components/Indexcomp'
+
+function App() {
+  return (
+    <div className="App">
+      <Boxs />
+    </div>
+  );
+}
+
+export default App;
